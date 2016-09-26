e9e7c46544dfacfddc891e2f5765fd3328ab2252
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2016-09-26 12:41:37 -0500

    Add underlined link, blue link mixins

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,4 +1,5 @@
-"use strict";
+import settings from "../settings.json";
+import { rgb } from "./color";
 
 /**
  * Adds outline styles
@@ -6,10 +7,67 @@
  * @return {Object}        CSS styles
  */
 function outline(offset = 2) {
-  return {
+  return typeof offset === "number" && {
     outline: "1px lightgray dotted",
     outlineOffset: `${offset}px`,
   };
 }
 
-export default outline;
+/**
+ * Creates a blue hyperlink; for use with inline styles via Radium
+ * @return {Object} CSS styles
+ */
+function blueLink() {
+  const hoverColor = "#4699d9";
+
+  return {
+    color: settings.color.blue,
+    textDecoration: "none",
+    transition: `color ${settings.timing.fast} ease-in-out`,
+
+    ":hover": { color: hoverColor },
+    ":active": { color: hoverColor },
+    ":focus": Object.assign({}, outline(), { color: hoverColor }),
+  };
+}
+
+/**
+ * Creates a nicely underlined hyperlink
+ * @param  {String} linkColor Link color
+ * @return {Object}           CSS styles object
+ */
+function underlinedLink(linkColor = settings.color.titleGray) {
+  const underlineOffset = 2;
+  const underlineWeight = 1;
+  const backgroundColor = settings.color.white;
+  const underlineColor = `rgba(${rgb(linkColor)}, .4)`;
+
+  return {
+    color: linkColor,
+    position: "relative",
+    textDecoration: "none",
+    transition: `color ${settings.timing.fast} ease`,
+
+    // Draw the underline with a background gradient
+    backgroundImage: `linear-gradient(
+      to top,
+      transparent,
+      transparent ${underlineOffset}px,
+      ${underlineColor} ${underlineOffset}px,
+      ${underlineColor} ${(underlineOffset + underlineWeight)}px,
+      transparent ${(underlineOffset + underlineWeight)}px
+    )`,
+
+    // Create breaks in the underline
+    textShadow: `-1px -1px 0 ${backgroundColor},
+      1px -1px 0 ${backgroundColor},
+      -1px 1px 0 ${backgroundColor},
+      1px 1px 0 ${backgroundColor}`,
+  };
+}
+
+export {
+  outline,
+  blueLink,
+  underlinedLink,
+};
