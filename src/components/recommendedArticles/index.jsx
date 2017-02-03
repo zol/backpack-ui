24dfee1a3c02d07e10050f7f55e1eccd237f7cf2
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2017-02-03 11:38:11 -0600

    Update RelatedArticles component (#92)
    * recommendedArticles: title, callout link now editable
    
    * Update RelatedArticles
    
    Replace `children` prop with `articles` prop which accepts an array of
    objects that define the articles.
    
    * Update stories

--- src/components/recommendedArticles/index.jsx
+++ src/components/recommendedArticles/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PropTypes } from "react";
 import radium, { Style } from "radium";
 import { media } from "../../../settings.json";
 import { gutter } from "../../utils/grid";
@@ -6,92 +6,98 @@ import Heading from "../heading";
 import CalloutLink from "../calloutLink";
 import ArticlePreview from "../articlePreview";
 
-function RecommendedArticles({ children }) {
-  const styles = {
-    container: {
+const styles = {
+  header: {
+    marginBottom: "43px",
+  },
+
+  heading: {
+    fontSize: "28px",
+    fontWeight: 100,
+    lineHeight: 1,
+  },
+
+  articles: {
+    [`@media (min-width: ${media.min["600"]})`]: {
+      display: "flex",
     },
+  },
 
-    childrenContainer: {
-      marginTop: "43px",
+  footer: {
+    marginTop: "39px",
+  },
+};
 
-      [`@media (min-width: ${media.min["600"]})`]: {
-        display: "flex",
-      },
-    },
-
-    heading: {
-      fontSize: "28px",
-      fontWeight: 100,
-      lineHeight: 1,
-    },
-
-    footer: {
-      marginTop: "39px",
-    },
-  };
-
-  return (
-    <section className="RecommendedArticles" style={styles.container}>
-      <Style
-        scopeSelector=".RecommendedArticles"
-        rules={{
-          mediaQueries: {
-            [`(max-width: ${media.max["600"]})`]: {
-              ".ArticlePreview + .ArticlePreview": {
-                marginTop: gutter("static"),
-              },
-            },
-
-            [`(min-width: ${media.min["600"]})`]: {
-              ".ArticlePreview + .ArticlePreview": {
-                marginLeft: gutter("static"),
-              },
+const RecommendedArticles = ({ articles, heading, calloutHref, calloutLabel, style }) => (
+  <section className="RecommendedArticles" style={style}>
+    <Style
+      scopeSelector=".RecommendedArticles"
+      rules={{
+        mediaQueries: {
+          [`(max-width: ${media.max["600"]})`]: {
+            ".ArticlePreview + .ArticlePreview": {
+              marginTop: gutter("static"),
             },
           },
-        }}
-      />
 
-      <header>
+          [`(min-width: ${media.min["600"]})`]: {
+            ".ArticlePreview + .ArticlePreview": {
+              marginLeft: gutter("static"),
+            },
+          },
+        },
+      }}
+    />
+
+    {heading &&
+      <header style={styles.header}>
         <Heading
           level={2}
           weight="thin"
           override={styles.heading}
         >
-          Recommended
+          {heading}
         </Heading>
       </header>
+    }
 
-      <div style={styles.childrenContainer}>
-        {children}
-      </div>
+    <div style={styles.articles}>
+      {articles.map((preview, index) => (
+        <ArticlePreview
+          title={preview.title}
+          paragraph={preview.paragraph}
+          image={preview.image}
+          href={preview.href}
+          category={preview.category}
+          categoryHref={preview.categoryHref}
+          key={index}
+        />
+      ))}
+    </div>
 
+    {calloutLabel && calloutHref &&
       <footer style={styles.footer}>
-        <CalloutLink href="/">
-          More recommendations
+        <CalloutLink href={calloutHref}>
+          {calloutLabel}
         </CalloutLink>
       </footer>
-    </section>
-  );
-}
+    }
+  </section>
+);
 
 RecommendedArticles.propTypes = {
-  children: (props, propName, componentName) => {
-    const prop = props[propName];
-    let error = null;
-
-    React.Children.forEach(prop, (child) => {
-      if (child.type !== ArticlePreview) {
-        error = new Error(`${componentName} children should be of type "ArticlePreview".`);
-      }
-
-      if (child.type === ArticlePreview && React.Children.count.length > 2) {
-        error = new Error(`${componentName} should have no more than 2 children.`);
-      }
-    });
-
-    return error;
-  },
+  articles: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    paragraph: PropTypes.string,
+    image: PropTypes.string,
+    href: PropTypes.string,
+    category: PropTypes.string,
+    categoryHref: PropTypes.string,
+  })).isRequired,
+  heading: PropTypes.string,
+  calloutLabel: PropTypes.string,
+  calloutHref: PropTypes.string,
+  style: PropTypes.objectOf(PropTypes.object),
 };
-
 
 export default radium(RecommendedArticles);
--- stories/index.jsx
+++ stories/index.jsx
@@ -1075,29 +1075,33 @@ storiesOf("Recommended articles", module)
   .addDecorator(withKnobs)
   .add("Default", () => (
     <StyleRoot>
-      <RecommendedArticles>
-        <ArticlePreview
-          title="New York’s most iconic buildings reimagined on deserted streets"
-          paragraph={`A new exhibition in New York of the city’s most iconic
-            buildings shows them in a new light, with the bustle of modern life
-            stripped out. Photographer`}
-          image="http://placehold.it/410x230"
-          href="/"
-          category="Art and culture"
-          categoryHref="/"
-        />
-
-        <ArticlePreview
-          title="Pull up a seat for David Attenborough’s Planet Earth II"
-          paragraph={`Ten years after the BBC series, Planet Earth, captivated a
-            global audience of over half a billion people, Planet Earth II is
-            coming to our TV screens, narrated once`}
-          image="http://placehold.it/410x230"
-          href="/"
-          category="Wildlife and nature"
-          categoryHref="/"
-        />
-      </RecommendedArticles>
+      <RecommendedArticles
+        heading={text("heading", "Recommended articles")}
+        calloutLabel={text("Callout label", "More recommendations")}
+        calloutHref={text("Callout URL", "/category/recommended")}
+        articles={array("Articles", [
+          {
+            title: "New York’s most iconic buildings reimagined on deserted streets",
+            paragraph: `A new exhibition in New York of the city’s most iconic
+              buildings shows them in a new light, with the bustle of modern life
+              stripped out. Photographer`,
+            image: "http://placehold.it/410x230",
+            href: "/",
+            category: "Art and culture",
+            categoryHref: "/",
+          },
+          {
+            title: "Pull up a seat for David Attenborough’s Planet Earth II",
+            paragraph: `Ten years after the BBC series, Planet Earth, captivated a
+              global audience of over half a billion people, Planet Earth II is
+              coming to our TV screens, narrated once`,
+            image: "http://placehold.it/410x230",
+            href: "/",
+            category: "Wildlife and nature",
+            categoryHref: "/",
+          },
+        ])}
+      />
     </StyleRoot>
   ));
 
