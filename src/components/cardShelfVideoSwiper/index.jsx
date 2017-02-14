90521a1f3a528edc5d1e1025f8b3254b1c56e9f0
Nick <nickfranciosi@gmail.com>
2017-02-14 16:10:18 -0600

    refactor to use component state (#124)

--- src/components/cardShelfVideoSwiper/index.jsx
+++ src/components/cardShelfVideoSwiper/index.jsx
@@ -70,58 +70,107 @@ const scopedStyles = {
   },
 };
 
-function CardShelfVideoSwiper({ children, heading, href, slidesVisible, style }) {
-  const sliderOptions = {
-    dots: false,
-    infinite: false,
-    slidesToShow: slidesVisible,
-    slidesToScroll: 1,
-    nextArrow: (<PaginatorButton direction="right" />),
-    prevArrow: (<PaginatorButton direction="left" />),
-    variableWidth: true,
-    swipe: false,
-    speed: 400,
-  };
+class CardShelfVideoSwiper extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <CardShelf
-      className="CardShelf--video"
-      style={style}
-    >
-      {heading &&
-        <CardShelfHeader
-          heading={heading}
-          href={href}
-        />
-      }
+    this.state = {
+      nextPagination: false,
+      prevPagination: false,
+    };
 
-      <div style={styles.slider}>
-        <Style
-          scopeSelector=".CardShelf--video"
-          rules={assign(
-            {},
-            scopedStyles,
-            slidesVisible === 3 && {
-              ".slick-slide + .slick-slide": {
-                marginLeft: "27px",
+    this.slideRefs = [];
+    this.hasEnoughSlides = this.hasEnoughSlides.bind(this);
+    this.checkAcitveSlide = this.checkAcitveSlide.bind(this);
+    this.renderPagination = this.renderPagination.bind(this);
+  }
+
+  componentDidMount() {
+    this.renderPagination();
+  }
+
+  getPagination(direction) {
+    return <PaginatorButton direction={direction} />;
+  }
+
+  checkAcitveSlide(index) {
+    return this.slideRefs[index] && this.slideRefs[index].classList.contains("slick-active");
+  }
+
+  hasEnoughSlides() {
+    return this.props.children.length >= this.props.slidesVisible;
+  }
+
+  renderPagination() {
+    const { children } = this.props;
+    if (this.hasEnoughSlides()) {
+      this.setState({
+        prevPagination: !this.checkAcitveSlide(0),
+        nextPagination: !this.checkAcitveSlide(children.length - 1),
+      });
+    }
+  }
+
+  render() {
+    const {
+      children,
+      heading,
+      href,
+      slidesVisible,
+      sliderOptions,
+      style,
+    } = this.props;
+    return (
+      <CardShelf
+        className="CardShelf--video"
+        style={style}
+      >
+        {heading &&
+          <CardShelfHeader
+            heading={heading}
+            href={href}
+          />
+        }
+
+        <div style={styles.slider}>
+          <Style
+            scopeSelector=".CardShelf--video"
+            rules={assign(
+              {},
+              scopedStyles,
+              slidesVisible === 3 && {
+                ".slick-slide + .slick-slide": {
+                  marginLeft: "27px",
+                },
               },
-            },
-            slidesVisible === 4 && {
-              ".slick-slide + .slick-slide": {
-                marginLeft: "43px",
+              slidesVisible === 4 && {
+                ".slick-slide + .slick-slide": {
+                  marginLeft: "43px",
+                },
               },
-            },
-          )}
-        />
+            )}
+          />
 
-        <Slider {...sliderOptions}>
-          {React.Children.map(children, (child, i) => (
-            <div key={i}>{child}</div>
-          ))}
-        </Slider>
-      </div>
-    </CardShelf>
-  );
+          <Slider
+            {...sliderOptions}
+            afterChange={this.renderPagination}
+            slidesToShow={slidesVisible}
+            nextArrow={this.state.nextPagination && this.getPagination("right")}
+            prevArrow={this.state.prevPagination && this.getPagination("left")}
+          >
+            {React.Children.map(children, (child, i) => (
+              <div
+                key={i}
+                ref={node => this.slideRefs[i] = node}
+              >
+                {child}
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </CardShelf>
+    );
+  }
 }
 
 CardShelfVideoSwiper.propTypes = {
@@ -140,6 +189,15 @@ CardShelfVideoSwiper.propTypes = {
   heading: PropTypes.string,
   href: PropTypes.string,
   slidesVisible: PropTypes.oneOf([3, 4]),
+  sliderOptions: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.bool,
+      PropTypes.element,
+      PropTypes.object,
+    ]),
+  ),
   style: PropTypes.objectOf(
     PropTypes.oneOfType([
       PropTypes.string,
@@ -151,6 +209,14 @@ CardShelfVideoSwiper.propTypes = {
 
 CardShelfVideoSwiper.defaultProps = {
   slidesVisible: 3,
+  sliderOptions: {
+    dots: false,
+    infinite: false,
+    slidesToScroll: 1,
+    variableWidth: true,
+    swipe: false,
+    speed: 400,
+  },
 };
 
 export default radium(CardShelfVideoSwiper);
