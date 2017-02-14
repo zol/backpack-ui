47cdf5d2d90eed1272635d7b5029a4cd4a012dc1
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2017-02-14 14:11:17 -0600

    Subdue card hover effect (#121)
    Remove shadow and scale from hover effect

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,19 +2,9 @@ import React, { PropTypes } from "react";
 import radium, { Style } from "radium";
 import cn from "classnames";
 import { rgba } from "../../utils/color";
-import { color, timing, zIndex } from "../../../settings.json";
+import { color } from "../../../settings.json";
 
-const cardHoverStyles = {
-  boxShadow: `0 28px 81px -7px ${rgba(color.black, 0.44)}`,
-  transform: "perspective(1px) scale(1.03)",
-  zIndex: (zIndex.top - 1),
-};
-
-const tileHoverStyles = {
-  ".Card-image": {
-    transform: "translateY(-2px)",
-  },
-
+const hoverStyles = {
   ".Heading": {
     color: `${color.blue} !important`,
   },
@@ -29,12 +19,6 @@ const styles = {
 
   card: {
     boxShadow: `0 12px 34px 0 ${rgba(color.black, 0.11)}`,
-    transition: `box-shadow ${timing.default} ease-in-out,
-      transform ${timing.default} ease-in-out`,
-
-    ":hover": cardHoverStyles,
-    ":active": cardHoverStyles,
-    ":focus": cardHoverStyles,
   },
 };
 
@@ -52,12 +36,10 @@ const Card = ({
       style,
     ]}
   >
-    {layout === "tile" &&
-      <Style
-        scopeSelector={`.Card:hover`}
-        rules={tileHoverStyles}
-      />
-    }
+    <Style
+      scopeSelector=".Card:hover"
+      rules={hoverStyles}
+    />
 
     {children}
   </div>
