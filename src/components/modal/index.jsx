cb373186506093c16839fac659eb4eb2f0901e88
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2017-05-08 11:38:50 -0500

    update: Clean up Modal component a bit (#278)
    * Add whitespace between code blocks
    
    * Remove outline from modal content
    
    * Remove generic "Modal" text from title, label
    
    * Use default timing value for closeTimeoutMs
    
    * Remove null children value since the prop is required
    
    * Remove isRequired from title prop
    
    A title isn’t always required, for example the log in modal
    
    * Move children propType validation to top
    
    List required props first
    
    * Import colors from styles
    
    * Import timing from styles
    
    * Import zIndex from styles
    
    * Import mq from styles
    
    * Import rgba utility
    
    * Change overlay opacity to 30%

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,27 +1,32 @@
 import React, { PropTypes } from "react";
 import Modal from "react-modal";
 import radium, { Style } from "radium";
-import { color, media, timing, zIndex } from "../../../settings.json";
+import colors from "../../styles/colors";
+import mq from "../../styles/mq";
+import timing from "../../styles/timing";
+import zIndex from "../../styles/zIndex";
 import Heading from "../heading";
-import { rgb } from "../../utils/color";
+import { rgba } from "../../utils/color";
 import propTypes from "../../utils/propTypes";
 
-const largeMQ = `(min-width: ${media.min["768"]})`;
+const largeMQ = `(min-width: ${mq.min["768"]})`;
 const modalPadding = 56;
 
 const styles = {
   overlay: {
-    backgroundColor: `rgba(${rgb(color.black)}, .4)`,
+    backgroundColor: rgba(colors.bgOverlay, 0.3),
     overflow: "hidden",
     zIndex: zIndex.modal - 2,
   },
+
   header: {
-    borderBottom: `1px solid ${color.gray}`,
+    borderBottom: `1px solid ${colors.borderPrimary}`,
     paddingBottom: "16px",
     paddingTop: "16px",
     position: "relative",
     textAlign: "center",
     textTransform: "uppercase",
+
     [`@media ${largeMQ}`]: {
       paddingLeft: `${modalPadding}px`,
       paddingRight: `${modalPadding}px`,
@@ -31,34 +36,43 @@ const styles = {
       textAlign: "left",
     },
   },
+
   contentContainer: {
     paddingLeft: `${modalPadding}px`,
     paddingRight: `${modalPadding}px`,
     paddingTop: "32px",
     paddingBottom: `${modalPadding * 2}px`,
   },
+
   actionItem: {
     position: "absolute",
     backgroundColor: "transparent",
     top: "12px",
+
     [`@media ${largeMQ}`]: {
       top: `${modalPadding}px`,
     },
   },
+
   rightAction: {
     right: "16px",
+
     [`@media ${largeMQ}`]: {
       right: `${modalPadding}px`,
     },
   },
+
   leftAction: {
     left: "16px",
+
     [`@media ${largeMQ}`]: {
       left: `${modalPadding}px`,
     },
   },
+
   desktopTitle: {
     display: "none",
+
     [`@media ${largeMQ}`]: {
       display: "block",
       textAlign: "center",
@@ -66,9 +80,11 @@ const styles = {
       paddingBottom: "104px",
     },
   },
+
   mobileTitle: {
     display: "block",
     minHeight: "10px",
+
     [`@media ${largeMQ}`]: {
       display: "none",
     },
@@ -90,36 +106,44 @@ function ModalComponent({
   style,
 }) {
   const rules = {
+    ".ReactModal__Content": {
+      outline: 0,
+    },
+
     ".ReactModal__Content--after-open.ReactModal__Content--before-close": {
       opacity: 0,
       transform: "translateY(55%)",
       transition: `opacity ${timing.default},
         transform ${timing.default}`,
     },
+
     ".ReactModal__Content--after-open": {
       opacity: "1 !important",
       transform: "tranlateY(0) !important",
       transition: `opacity ${timing.default},
         transform ${timing.default}`,
     },
+
     ".ReactModal__Overlay--after-open.ReactModal__Overlay--before-close": {
       opacity: 0,
       transition: `opacity ${timing.default},
         transform ${timing.default}`,
     },
+
     ".ReactModal__Overlay--after-open": {
       opacity: 1,
       transition: `opacity ${timing.default},
         transform ${timing.default}`,
     },
+
     ".ModalBase": {
-      background: color.white,
+      background: colors.bgPrimary,
       position: "absolute",
       overflow: "auto",
       WebkitOverflowScrolling: "touch",
       border: 0,
       borderRadius: 0,
-      boxShadow: `0 27px 50px rgba(${rgb(color.black)}, .36)`,
+      boxShadow: `0 27px 50px ${rgba(colors.bgOverlay, 0.36)}`,
       top: 0,
       left: 0,
       right: 0,
@@ -130,6 +154,7 @@ function ModalComponent({
       height: "100vh",
       width: "100%",
     },
+
     mediaQueries: {
       [largeMQ]: {
         ".ModalBase": {
@@ -155,6 +180,7 @@ function ModalComponent({
         scopeSelector=".ReactModalPortal"
         rules={rules}
       />
+
       <header
         className="Modal-header clearfix"
         style={styles.header}
@@ -167,6 +193,7 @@ function ModalComponent({
             {leftActionContent}
           </button>
         }
+
         <Heading
           level={4}
           size="small"
@@ -201,6 +228,7 @@ function ModalComponent({
             {title}
           </Heading>
         }
+
         {children}
       </div>
     </Modal>
@@ -209,6 +237,10 @@ function ModalComponent({
 
 ModalComponent.propTypes = {
   isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.string,
+  ]).isRequired,
   closeModal: PropTypes.func,
   leftAction: PropTypes.func,
   leftActionContent: PropTypes.node,
@@ -217,21 +249,14 @@ ModalComponent.propTypes = {
   closeTimeoutMS: PropTypes.number,
   contentLabel: PropTypes.string,
   desktopWidth: PropTypes.string,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   style: propTypes.style,
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.string,
-  ]).isRequired,
 };
 
 ModalComponent.defaultProps = {
   isOpen: false,
-  title: "Modal",
   desktopWidth: "85%",
-  contentLabel: "Modal",
-  closeTimeoutMS: 500,
-  children: null,
+  closeTimeoutMS: timing.default,
 };
 
 ModalComponent.styles = styles;
