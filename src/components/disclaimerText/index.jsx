9ef79052252d374d583d016a00b0d622937faf79
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2017-05-10 13:58:42 -0500

    fix: Add hover, active, focus to DisclaimerText link (#286)

--- src/components/disclaimerText/index.jsx
+++ src/components/disclaimerText/index.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from "react";
 import radium, { Style } from "radium";
 import colors from "../../styles/colors";
+import timing from "../../styles/timing";
 import font from "../../utils/font";
 import { fontWeightMedium } from "../../styles/typography";
 import propTypes from "../../utils/propTypes";
@@ -27,6 +28,19 @@ const DisclaimerText = ({ children, style }) => (
       rules={{
         ".DisclaimerText a": {
           fontWeight: fontWeightMedium,
+          transition: `color ${timing.fast} ease-in-out`,
+        },
+
+        ".DisclaimerText a:hover": {
+          color: colors.linkPrimaryHover,
+        },
+
+        ".DisclaimerText a:active": {
+          color: colors.linkPrimaryHover,
+        },
+
+        ".DisclaimerText a:focus": {
+          color: colors.linkPrimaryHover,
         },
       }}
     />
