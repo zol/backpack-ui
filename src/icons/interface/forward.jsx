7e091aca9ebf4fc5be2918e027026f55775ad89b
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-05-15 14:39:50 -0500

    Add interface icons

---
+++ src/icons/interface/forward.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+import React from "react";
+import Icon from "../../components/icon";
+
+const Back = props => (
+  <Icon {...props}>
+    <path d="M13.6 0v22.3l-9-9.4-3.5 3.3 14.9 15.8 14.9-15.8-3.5-3.3-9 9.4v-22.3z" transform="rotate(90 16 16)" />
+  </Icon>
+);
+
+export default Back;
---
+++ src/icons/interface/settings.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+import React from "react";
+import Icon from "../../components/icon";
+
+const Settings = props => (
+  <Icon {...props}>
+    <path d="" />
+  </Icon>
+);
+
+export default Settings;
