9108d06bc8cf8c2d34132a580124f6f53dbdefca
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-05-15 14:43:55 -0500

    Replace index.jsx with index.js

---
+++ src/icons/index.js
@@ -0,0 +1,121 @@
+import Back from "./interface/back";
+import Caret from "./interface/caret";
+import Close from "./interface/close";
+import Edit from "./interface/edit";
+import Forward from "./interface/forward";
+import Heart from "./interface/heart";
+import Images from "./interface/images";
+import Launch from "./interface/launch";
+import Link from "./interface/link";
+import Menu from "./interface/menu";
+import Overflow from "./interface/overflow";
+import Phone from "./interface/phone";
+import Search from "./interface/search";
+import Share from "./interface/share";
+import Timestamp from "./interface/timestamp";
+import Website from "./interface/website";
+
+import Drink from "./map/drink";
+import Eat from "./map/eat";
+import Play from "./map/play";
+import See from "./map/see";
+import Shop from "./map/shop";
+import Sleep from "./map/sleep";
+import Transport from "./map/transport";
+
+import Apple from "./social/apple";
+import Facebook from "./social/facebook";
+import FacebookColor from "./social/facebookColor";
+import FacebookBlock from "./social/facebookBlock";
+import FacebookBlockColor from "./social/facebookBlockColor";
+import FacebookMessenger from "./social/facebookMessenger";
+import FacebookMessengerColor from "./social/facebookMessengerColor";
+import Google from "./social/google";
+import GoogleColor from "./social/googleColor";
+import Instagram from "./social/instagram";
+import Pinterest from "./social/pinterest";
+import PinterestColor from "./social/pinterestColor";
+import Reddit from "./social/reddit";
+import RedditColor from "./social/redditColor";
+import Twitter from "./social/twitter";
+import TwitterColor from "./social/twitterColor";
+
+import Airplane from "./survivalGuide/airplane";
+import Bear from "./survivalGuide/bear";
+import Bed from "./survivalGuide/bed";
+import BookPencil from "./survivalGuide/bookPencil";
+import Car from "./survivalGuide/car";
+import Health from "./survivalGuide/health";
+import Lamp from "./survivalGuide/lamp";
+import Lgbt from "./survivalGuide/lgbt";
+import Money from "./survivalGuide/money";
+import Passport from "./survivalGuide/passport";
+import Safety from "./survivalGuide/safety";
+import Speech from "./survivalGuide/speech";
+import Suitcase from "./survivalGuide/suitcase";
+import Umbrella from "./survivalGuide/umbrella";
+import Vespa from "./survivalGuide/vespa";
+import Visa from "./survivalGuide/visa";
+import Wheelchair from "./survivalGuide/wheelchair";
+
+export default {
+  Back,
+  Caret,
+  Close,
+  Edit,
+  Forward,
+  Heart,
+  Images,
+  Launch,
+  Link,
+  Menu,
+  Overflow,
+  Phone,
+  Search,
+  Share,
+  Timestamp,
+  Website,
+
+  Drink,
+  Eat,
+  Play,
+  See,
+  Shop,
+  Sleep,
+  Transport,
+
+  Apple,
+  Facebook,
+  FacebookColor,
+  FacebookBlock,
+  FacebookBlockColor,
+  FacebookMessenger,
+  FacebookMessengerColor,
+  Google,
+  GoogleColor,
+  Instagram,
+  Pinterest,
+  PinterestColor,
+  Reddit,
+  RedditColor,
+  Twitter,
+  TwitterColor,
+
+  Airplane,
+  Bear,
+  Bed,
+  BookPencil,
+  Car,
+  Health,
+  Lamp,
+  Lgbt,
+  Money,
+  Passport,
+  Safety,
+  Speech,
+  Suitcase,
+  Umbrella,
+  Vespa,
+  Visa,
+  Wheelchair,
+};
