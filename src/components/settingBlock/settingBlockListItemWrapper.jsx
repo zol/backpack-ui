f0d77d2cd9f699cd817ad43f240b9140325b654c
Nick <nickfranciosi@gmail.com>
2017-06-29 10:46:08 -0500

    fix: add passthrough to listItemWrapper (#358)

--- src/components/settingBlock/settingBlockListItemWrapper.jsx
+++ src/components/settingBlock/settingBlockListItemWrapper.jsx
@@ -1,4 +1,7 @@
-import React, { PropTypes } from "react";
+import React from "react";
+import radium from "radium";
+import PropTypes from "prop-types";
+import propTypes from "../../utils/propTypes";
 
 const styles = {
   base: {
@@ -6,10 +9,10 @@ const styles = {
   },
 };
 
-const SettingBlockListItemWrapper = ({ children }) => (
+const SettingBlockListItemWrapper = ({ children, style }) => (
   <div
     className="SettingBlockListItemWrapper"
-    style={styles.base}
+    style={[styles.base, style]}
   >
     {children}
   </div>
@@ -17,6 +20,7 @@ const SettingBlockListItemWrapper = ({ children }) => (
 
 SettingBlockListItemWrapper.propTypes = {
   children: PropTypes.element,
+  style: propTypes.style,
 };
 
-export default SettingBlockListItemWrapper;
+export default radium(SettingBlockListItemWrapper);
