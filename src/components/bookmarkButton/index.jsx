43e6b4ef1e5726bb1f4080476910e7f07f71efed
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2017-08-24 14:18:23 -0500

    fix: Remove iconType, responsive styles from BookmarkButton (#453)

--- src/components/bookmarkButton/index.jsx
+++ src/components/bookmarkButton/index.jsx
@@ -1,46 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import radium from "radium";
-import mq from "../../styles/mq";
-import { fontSizeBodyArticle } from "../../styles/typography";
 import ListButton from "../listButton";
 import propTypes from "../../utils/propTypes";
 
-const BookmarkButton = ({ onClick, marked, iconType, style }) => {
-  const styles = {
-    [`@media (min-width: ${mq.max["768"]})`]: {
-      boxShadow: "none",
-      display: "flex",
-      fontSize: fontSizeBodyArticle,
-    },
-  };
-
-  const icons = {
-    default: ["BookmarkActive", "Bookmark"],
-    alternate: ["BookmarkAltActive", "BookmarkAlt"],
-  };
-
-  return (
-    <ListButton
-      onClick={onClick}
-      label={marked ? "Remove from list" : "Save to list"}
-      icon={marked ? icons[iconType][0] : icons[iconType][1]}
-      style={[styles, style]}
-    />
-  );
-};
+const BookmarkButton = ({ onClick, marked, style }) => (
+  <ListButton
+    onClick={onClick}
+    label="Save to list"
+    icon={marked ? "BookmarkActive" : "Bookmark"}
+    style={style}
+  />
+);
 
 BookmarkButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   marked: PropTypes.bool,
-  iconType: PropTypes.oneOf(["default", "alternate"]),
   style: propTypes.style,
 };
 
 BookmarkButton.defaultProps = {
   onClick: null,
   marked: false,
-  iconType: "default",
   style: null,
 };
 
